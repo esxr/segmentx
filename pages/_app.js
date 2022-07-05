f860f5046a0c0eb716426e6355e1fc3d277dc256
Pranav Dhoolia <dhoolia.pranav@gmail.com>
2022-07-05 16:05:43 +1000

    Make reportWebVitals function to measure performance

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
 import '../styles/globals.scss'
 
+// Make reportWebVitals function to measure performance
+export function reportWebVitals(metric) {
+  switch (metric.name) {
+    case 'FCP':
+      // handle FCP results
+      break
+    case 'LCP':
+      // handle LCP results
+      break
+    case 'CLS':
+      // handle CLS results
+      break
+    case 'FID':
+      // handle FID results
+      break
+    case 'TTFB':
+      // handle TTFB results
+      break
+    default:
+      break
+  }
+}
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
